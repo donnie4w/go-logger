8f082862740ee1082af95fecca7bc6e67f21f035
szqh97 <szqh97@163.com>
2014-03-28 16:50:10 +0800

    fix bug: FileMode is set to 0 in OpenFile

--- logger/logger.go
+++ logger/logger.go
@@ -81,7 +81,7 @@ func SetRollingFile(fileDir, fileName string, maxNumber int32, maxSize int64, _u
 		}
 	}
 	if !logObj.isMustRename() {
-		logObj.logfile, _ = os.OpenFile(fileDir+"/"+fileName, os.O_RDWR|os.O_APPEND|os.O_CREATE, 0)
+		logObj.logfile, _ = os.OpenFile(fileDir+"/"+fileName, os.O_RDWR|os.O_APPEND|os.O_CREATE, 0644)
 		logObj.lg = log.New(logObj.logfile, "\n", log.Ldate|log.Ltime|log.Lshortfile)
 	} else {
 		logObj.rename()
