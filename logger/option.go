70302a2f65f8782fe1ac97fe11c1a486117decc9
donnie4w <donnie4w@gmail.com>
2024-08-08 09:43:24 +0800

    Update option.go

--- logger/option.go
+++ logger/option.go
@@ -72,16 +72,29 @@ func (f *FileTimeMode) Compress() bool {
 	return f.IsCompress
 }
 
+// Option represents a configuration option for the Logging struct.
+// It includes various settings such as log level, console output, format, formatter, file options, and a custom handler.
 type Option struct {
-	Level         _LEVEL
-	Console       bool
-	Format        _FORMAT
-	Formatter     string
-	FileOption    FileOption
-	CustomHandler func(lc *LogContext) bool
+	Level      _LEVEL     // Log level, e.g., DEBUG, INFO, WARN, ERROR, etc.
+	Console    bool       // Whether to also output logs to the console.
+	Format     _FORMAT    // Log format.
+	Formatter  string     // Formatting string for customizing the log output format.
+	FileOption FileOption // File-specific options for the log handler.
+	Stacktrace _LEVEL     // Log level, e.g., DEBUG, INFO, WARN, ERROR, etc.
+	// CustomHandler
+	//
+	// When customHandler returns false, the println function returns without executing further prints. Returning true allows the subsequent print operations to continue.
+	//
+	// customHandler返回false时，println函数返回，不再执行后续的打印，返回true时，继续执行后续打印。
+	CustomHandler func(lc *LogContext) bool // Custom log handler function allowing users to define additional log processing logic.
 }
 
 type LogContext struct {
 	Level _LEVEL
 	Args  []any
+}
+
+type LevelOption struct {
+	Format    _FORMAT // Log format.
+	Formatter string  // Formatting string for customizing the log output format.
 }
