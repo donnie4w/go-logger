b1c25204c6d55201ec258d7c858383b8880b8a80
donnie4w <donnie4w@gmail.com>
2024-10-26 11:27:10 +0800

    Update stack.go

--- logger/stack.go
+++ logger/stack.go
@@ -1,18 +1,33 @@
-// Copyright (c) 2023, donnie <donnie4w@gmail.com>
+// Copyright (c) 2014, donnie <donnie4w@gmail.com>
 // All rights reserved.
 // Use of t source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
+//
+// github.com/donnie4w/go-logger
 
 package logger
 
 import (
 	"github.com/donnie4w/gofer/buffer"
+	pool "github.com/donnie4w/gofer/pool/buffer"
 	"runtime"
 )
 
 type callStack struct {
-	stack []callerInfo
+	stack []*callerInfo
 }
+
+func (c *callStack) reset() {
+	if len(c.stack) > 0 {
+		c.stack = c.stack[:0]
+	}
+}
+
+var callStackPool = pool.NewPool[callStack](func() *callStack {
+	return &callStack{stack: make([]*callerInfo, 0)}
+}, func(c *callStack) {
+	c.reset()
+})
 
 type callerInfo struct {
 	FileName string
@@ -20,12 +35,26 @@ type callerInfo struct {
 	FuncName string
 }
 
+func (c *callerInfo) reset() {
+	c.FileName, c.FuncName = "", ""
+}
+
+var callerInfoPool = pool.NewPool[callerInfo](func() *callerInfo {
+	return &callerInfo{}
+}, func(c *callerInfo) {
+	c.reset()
+})
+
 func (cs *callStack) Push(fileName string, line int) {
-	cs.stack = append(cs.stack, callerInfo{FileName: fileName, Line: line})
+	ci := callerInfoPool.Get()
+	ci.FileName, ci.Line = fileName, line
+	cs.stack = append(cs.stack, ci)
 }
 
 func (cs *callStack) PushWithFunc(fileName string, line int, funcName string) {
-	cs.stack = append(cs.stack, callerInfo{FileName: fileName, Line: line, FuncName: funcName})
+	ci := callerInfoPool.Get()
+	ci.FileName, ci.Line, ci.FuncName = fileName, line, funcName
+	cs.stack = append(cs.stack, ci)
 }
 
 func (cs *callStack) Pop(flag _FORMAT, fileBuffer *buffer.Buffer) {
@@ -34,6 +63,7 @@ func (cs *callStack) Pop(flag _FORMAT, fileBuffer *buffer.Buffer) {
 		if i < len(cs.stack)-1 {
 			fileBuffer.WriteByte('#')
 		}
+		callerInfoPool.Put(&ci)
 	}
 	return
 }
@@ -43,7 +73,7 @@ func collectCallStack(depth int, formatfunc bool, stack *callStack, recursion bo
 		return stack
 	}
 	if stack == nil {
-		stack = &callStack{make([]callerInfo, 0)}
+		stack = callStackPool.Get()
 	}
 	i := 1
 	for {
@@ -51,9 +81,8 @@ func collectCallStack(depth int, formatfunc bool, stack *callStack, recursion bo
 		if more := runtime.Callers(depth+i, pcs[:]); more == 0 {
 			return stack
 		}
-		var f runtime.Frame
-		var ok bool
-		if f, ok = m.Get(pcs[0]); !ok {
+		f, ok := m.Get(pcs[0])
+		if !ok {
 			f, _ = runtime.CallersFrames([]uintptr{pcs[0]}).Next()
 			m.Put(pcs[0], f)
 		}
@@ -67,6 +96,41 @@ func collectCallStack(depth int, formatfunc bool, stack *callStack, recursion bo
 		}
 		i++
 	}
-
 	return stack
 }
+
+//func collectCallStack(depth int, formatfunc bool, stack *callStack, recursion bool) *callStack {
+//	if depth <= 0 {
+//		return stack
+//	}
+//
+//	pcs := make([]uintptr, 0, 8)
+//	n := runtime.Callers(depth+1, pcs[:cap(pcs)])
+//	for n == cap(pcs) {
+//		newCap := cap(pcs) + 8
+//		pcs = make([]uintptr, 0, newCap)
+//		n = runtime.Callers(depth+1, pcs[:cap(pcs)])
+//	}
+//
+//	if stack == nil {
+//		stack = callStackPool.Get()
+//	}
+//
+//	pcs = pcs[:n]
+//	for i := 0; i < n; i++ {
+//		f, ok := m.Get(pcs[i])
+//		if !ok {
+//			f, _ = runtime.CallersFrames(pcs[i : i+1]).Next()
+//			m.Put(pcs[i], f)
+//		}
+//		if formatfunc {
+//			stack.PushWithFunc(f.File, f.Line, funcname(f.Func.Name()))
+//		} else {
+//			stack.Push(f.File, f.Line)
+//		}
+//		if !recursion {
+//			break
+//		}
+//	}
+//	return stack
+//}
