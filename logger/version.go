e338c05951211ebb71845c7b95e4f6d10377c7e3
donnie4w <donnie4w@gmail.com>
2024-10-26 11:27:21 +0800

    Create version.go

---
+++ logger/version.go
@@ -0,0 +1,12 @@
+// Copyright (c) 2014, donnie <donnie4w@gmail.com>
+// All rights reserved.
+// Use of t source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+//
+// github.com/donnie4w/go-logger
+
+package logger
+
+const (
+	VERSION string = "0.27.0"
+)
