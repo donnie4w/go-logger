04c6434485c0d80c05baa936d734a7100ef5ce45
donnie4w <donnie4w@gmail.com>
2024-11-22 11:58:53 +0800

    Update 0_25_0_test.go

--- test/0_25_0_test.go
+++ test/0_25_0_test.go
@@ -11,14 +11,21 @@ func TestOption4time(t *testing.T) {
 	for i := 0; i < 100; i++ {
 		logger.Debug("this is a debug message", 1111111111111111111)
 		logger.Info("this is a info message", 2222222222222222222)
+		logger.Warn("this is a warn message", 33333333333333333)
+		logger.Error("this is a error message", 4444444444444444444)
+		logger.Fatal("this is a fatal message", 555555555555555555)
 		time.Sleep(2 * time.Second)
 	}
 }
 
 func TestOption4size(t *testing.T) {
-	logger.SetOption(&logger.Option{Level: logger.LEVEL_DEBUG, Console: true, FileOption: &logger.FileSizeMode{Filename: "testlog.log", Maxsize: 500, Maxbuckup: 3, IsCompress: false}})
+	logger.SetOption(&logger.Option{Level: logger.LEVEL_DEBUG, Console: true, FileOption: &logger.FileSizeMode{Filename: "testlog.log", Maxsize: 1 << 10, Maxbuckup: 10, IsCompress: false}})
 	for i := 0; i < 20; i++ {
 		logger.Debug("this is a debug message", 1111111111111111111)
-		time.Sleep(100 * time.Millisecond)
+		logger.Info("this is a info message", 2222222222222222222)
+		logger.Warn("this is a warn message", 33333333333333333)
+		logger.Error("this is a error message", 4444444444444444444)
+		logger.Fatal("this is a fatal message", 555555555555555555)
 	}
+	time.Sleep(1 * time.Second)
 }
